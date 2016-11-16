beab054afa09a9fe51169dc81ba05852c36faed1
David Bimamisa <info@shaka-b.de>
2016-11-16 22:36:53 +0100

    Consume collapse events

--- src/js/components/capability_table.js
+++ src/js/components/capability_table.js
@@ -11,18 +11,21 @@ export class CapabilityTableComponent extends RenderComponent {
         this.updateModel(viewModel);
     }
 
-    onRenderingStarted(){
+    onRenderingStarted() {
         this._createSubComponents();
         this._onCollapseTable();
+        this._consumeTableCollapse();
     }
 
-    onRendering(){
+    onRendering() {
         this.engineInfoPopover.onRendering();
         this.defaultResultPopover.onRendering();
         this.defaultTestPopover.onRendering();
+        this._onCollapseTable();
+        this._consumeTableCollapse();
     }
 
-    _createSubComponents(){
+    _createSubComponents() {
 
         /*            buildEngineInfoPopover();
          onCollapseFeatureTable();
@@ -32,15 +35,15 @@ export class CapabilityTableComponent extends RenderComponent {
          buildFeaturePopover();*/
 
         this.engineInfoPopover = new EngineInfoPopover({
-            engines:  this.viewModel.engines
+            engines: this.viewModel.engines
         });
 
-        if(isPerformanceCapability(this.viewModel.capability)) {
+        if (isPerformanceCapability(this.viewModel.capability)) {
 
             this.defaultTestPopover = new FeatureTestPopover({
                 capability: this.viewModel.capability,
                 features: this.viewModel.features,
-                independentTests:  this.viewModel.independentTests,
+                independentTests: this.viewModel.independentTests,
                 title: 'Performance-Test',
                 id: '[data-feature-index].info-exp-feature'
             });
@@ -58,12 +61,12 @@ export class CapabilityTableComponent extends RenderComponent {
             this.defaultTestPopover = new FeatureTestPopover({
                 capability: this.viewModel.capability,
                 features: this.viewModel.features,
-                independentTests:  this.viewModel.independentTests,
+                independentTests: this.viewModel.independentTests,
                 title: 'Feature-Test'
             });
 
             this.defaultResultPopover = new FeatureResultPopover({
-                capability: this.viewModel.capability ,
+                capability: this.viewModel.capability,
                 features: this.viewModel.features,
                 title: 'Feature-Result',
             });
@@ -73,7 +76,7 @@ export class CapabilityTableComponent extends RenderComponent {
 
     }
 
-    updateModel(viewModel, preventRendering){
+    updateModel(viewModel, preventRendering) {
         var featureTitleColspan = viewModel.engines.length + 1;
         if (isExpressivenessCapability(viewModel.capability)) {
             featureTitleColspan = featureTitleColspan + 1;
@@ -86,7 +89,7 @@ export class CapabilityTableComponent extends RenderComponent {
         this.viewModel = viewModel;
         this.context = viewModel.table;
         this.context['featureTitleColspan'] = featureTitleColspan;
-        if(!preventRendering || preventRendering == undefined){
+        if (!preventRendering || preventRendering == undefined) {
             super.render();
         }
 
@@ -94,40 +97,80 @@ export class CapabilityTableComponent extends RenderComponent {
     }
 
 
-    _onCollapseTable(){
+    _onCollapseTable() {
 
-    $('.row-feat-title').on('show.bs.collapse', function(){
+        $('.row-feat-title').on('show.bs.collapse', function () {
+            var tr = $(this).prev();
+            if (tr.length == 1) {
+                tr.addClass('row-expanded');
 
-        var tr = $(this).prev();
-        if(tr.length == 1){
-            tr.addClass('row-expanded');
+                var expendIcon = tr.find('.entypo-right-open');
 
-            var expendIcon = tr.find('.entypo-right-open');
-
-            if(expendIcon.length == 1){
-                expendIcon.removeClass('entypo-right-open');
-                expendIcon.addClass('entypo-down-open');
+                if (expendIcon.length == 1) {
+                    expendIcon.removeClass('entypo-right-open');
+                    expendIcon.addClass('entypo-down-open');
+                }
             }
-        }
 
-    });
+        });
 
-    $('.row-feat-title').on('hidden.bs.collapse', function(){
+        $('.row-feat-title').on('hidden.bs.collapse', function () {
+            var tr = $(this).prev();
+            if (tr.length == 1) {
+                tr.removeClass('row-expanded');
 
-        var tr = $(this).prev();
-        if(tr.length == 1){
-            tr.removeClass('row-expanded');
-
-            var expendIcon = tr.find('.entypo-down-open');
-            if(expendIcon.length == 1){
-                expendIcon.removeClass('entypo-down-open');
-                expendIcon.addClass('entypo-right-open');
+                var expendIcon = tr.find('.entypo-down-open');
+                if (expendIcon.length == 1) {
+                    expendIcon.removeClass('entypo-down-open');
+                    expendIcon.addClass('entypo-right-open');
+                }
             }
+
+        });
+    }
+
+    _consumeTableCollapse() {
+        // Prevent the table row to collapse when clicking on a icon
+        if (isPerformanceCapability(this.viewModel.capability)) {
+
+            $('.info-exp-feature').on('click', function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+            });
+
+            $('[data-test-info].info-performance-test').on('click', function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+            });
+
+            $('.collapse-parent-row').on('click', function (e) {
+                // we check if popover is visible by looking for the class .popover-content
+                if ($('.popover-content').is(':visible')) {
+                    $('[data-test-index].info-performance-test').popover('hide');
+                    $('[data-feature-index].info-exp-feature').popover('hide');
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+            });
+
+
+        } else {
+            $('.construct-info').on('click', function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+            });
+
+            $('.collapse-parent-row').on('click', function (e) {
+                if ($('.popover-content').is(':visible')) {
+                    $('[data-test-index].info-engine-test').popover('hide');
+                    $('[data-feature-index].info-feature').popover('hide');
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+
+            });
         }
-
-    });
-
-}
+    }
 
 
 }
